1b03b53df487cbd3ff782a3524b7efd084f1bd4a
Dan Chiev <danchiev2@Gmail.com>
2021-04-13 18:50:55 -0400

    added link token

--- routes/api/plaid.js
+++ routes/api/plaid.js
@@ -11,11 +11,30 @@ const Account = require("../../models/Account");
 const User = require("../../models/User");
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
-const PLAID_PUBLIC_KEY = "YOUR_PUBLIC_KEY";
+const response = await client
+  .createLinkToken({
+    user: {
+      client_user_id: "123-test-user-id",
+    },
+    client_name: "Plaid Test App",
+    products: ["auth", "transactions"],
+    country_codes: ["GB"],
+    language: "en",
+    webhook: "https://sample-web-hook.com",
+    account_filters: {
+      depository: {
+        account_subtypes: ["checking", "savings"],
+      },
+    },
+  })
+  .catch((err) => {
+    // handle error
+  });
+const linkToken = response.link_token;
 const client = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
-  PLAID_PUBLIC_KEY,
+  linkToken,
   plaid.environments.sandbox,
   { version: "2018-05-22" }
 );

e47bac131558fd03b7dcbe2792873473a73c616b
Dan Chiev <danchiev2@Gmail.com>
2021-04-13 18:57:31 -0400

    corrected typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 require("dotenv").config();
 const users = require("./routes/api/users");
-const plaid = require(".routes/api/plaid");
+const plaid = require("./routes/api/plaid");
 
 const app = express();
 
@@ -33,7 +33,7 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
-app.use("api/routes", plaid);
+app.use("/api/routes", plaid);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
